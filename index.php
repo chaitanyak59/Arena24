1a1eee79b1b59d69ad9fe9c44389d09e5ed167c3
chaitanyak59 <chaitanyak59@gmail.com>
2021-12-07 21:04:40 +0530

    alignment

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-require_once realpath("vendor/autoload.php");
-include_once "src/templates/header.php";
-include_once "src/templates/nav.php";
-include_once "src/templates/body.php";
-include_once "src/templates/footer.php";
+    require_once realpath("vendor/autoload.php");
+    include_once "src/templates/header.php";
+    include_once "src/templates/nav.php";
+    include_once "src/templates/body.php";
+    include_once "src/templates/footer.php";
 ?>
--- logout.php
+++ logout.php
@@ -1,4 +1,4 @@
 <?php
-require_once realpath("vendor/autoload.php");
-include_once "src/logout-page.php";
+    require_once realpath("vendor/autoload.php");
+    include_once "src/logout-page.php";
 ?>
